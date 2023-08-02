3d13a87287ce84f1ffc5475d727ebaba7b574f23
Cody Boone Ferguson <53008573+xexyl@users.noreply.github.com>
2023-08-02 04:20:40 -0700

    Update DBG_VERSION in dbg.h

--- dbg.h
+++ dbg.h
@@ -41,7 +41,7 @@
 /*
  * definitions
  */
-#define DBG_VERSION "2.10 2023-08-01"		/* format: major.minor YYYY-MM-DD */
+#define DBG_VERSION "2.11 2023-08-02"		/* format: major.minor YYYY-MM-DD */
 
 
 /*
